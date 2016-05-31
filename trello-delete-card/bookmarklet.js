0bd583c1bace3fa1951307b0df237724e06032d4
Anand Graves <anandgraves@users.noreply.github.com>
2016-05-31 07:21:14 +0200

    Changed script url

--- trello-delete-card/bookmarklet.js
+++ trello-delete-card/bookmarklet.js
@@ -3,7 +3,7 @@ javascript: (function() {
         TrelloDeleteCard.init();
     } else {
         var script = document.createElement('script');
-        script.src = 'https://cdn.rawgit.com/anandgraves/trello-delete-card/master/trello-delete-card.js?' + Math.random().toString(36).substring(8);
+        script.src = 'https://cdn.rawgit.com/anandgraves/anandgraves.github.io/master/trello-delete-card/assets/js/trello-delete-card.js?' + Math.random().toString(36).substring(8);
         script.onload = function() {
             TrelloDeleteCard.init();
         };
